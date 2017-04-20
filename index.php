fa1e015541a1b6ac70a185810480ec9d5d2e7fec
ohnn <henrinikku1@gmail.com>
2017-04-20 10:36:38 +0300

    tablesorter

--- index.php
+++ index.php
@@ -77,6 +77,6 @@ ini_set('display_errors', 'on');
 
         <script src="js/main.js"></script>
         <script src="js/ajax.js"></script>
-        <script src="tablesorter/jquery.tablesorter.js"></script>
+        <script src="js/tablesorter/jquery.tablesorter.js"></script>
     </body>
 </html>
